6c290e5166dafe8c44c8feb6d140e12281dc8928
ahmedkhaled98 <ahmedkhaled199821@yahoo.com>
2022-04-04 10:43:33 +0200

    update-form modified

--- src/components/updateform.vue
+++ src/components/updateform.vue
@@ -12,7 +12,7 @@
        <input type="text" placeholder="enter your last-name" v-model="lname" ><br>
        <input type="email" placeholder="email"  v-model="email"><br>
        <input type="text" placeholder="gender"  v-model="gender"><br>
-         <button class="button-63 px-5" @click.prevent="updateinfo ,showmsg=true"  >UPDATE</button>
+         <button class="button-63 px-5" @click.prevent="updateinfo" @click="showmsg=true"  >UPDATE</button>
       </form>
       <div v-if="showmsg" id="myDIV" class="alert alert-success mt-2 ml-5 w-50 text-center" role="alert">
            <p> Student updated successfuly </p>
@@ -40,20 +40,11 @@
         }
     },
       created(){
-             
-             
              this.getbyid()
              console.log(this.fname)
             },
         methods:{
-        //    msg(){
-        //        var x = document.getElementById("myDIV");
-        //     if (x.style.display === "none") {
-        //            x.style.display = "block";
-        //         } else {
-        //                x.style.display = "none";
-        //           }
-        //    },
+       
         updateinfo(){
             
             const updateduser={}
